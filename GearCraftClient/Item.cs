cf602841106c340d11115408f8c58120c6373d79
Daniel Alexander Nedergaard <daniel.nedergaard@uptime.dk>
2018-02-13 13:04:21 +0100

    Added an itemrarity enum and some item methods

--- GearCraftClient/Item.cs
+++ GearCraftClient/Item.cs
@@ -9,5 +9,37 @@ namespace GearCraftClient
     class Item
     {
         public int RequiredLevel { get; set; }
+        public string Rarity { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public List<Stat> Stats { get; set; }
+
+        public void GenerateItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        private string AddRandomRarity()
+        {
+            throw new NotImplementedException();
+        }
+
+        private string AddRandomStat(int itemLevel, string itemRarity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum Rarity
+    {
+        LowQuality,
+        Magical,
+        Rare,
+        Unique
     }
 }
